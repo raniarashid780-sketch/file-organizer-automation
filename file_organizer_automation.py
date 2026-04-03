64003779acf80c7af17f83daae3678137c1ec494
Rania Rashid <raniarashid780@gmail.com>
2026-04-03 05:56:59 -0700

    Update file_organizer_automation.py

--- file_organizer_automation.py
+++ file_organizer_automation.py
@@ -1,9 +1,11 @@
 from pathlib import Path
 
 def create_destination(base, file_type_folder, date_folder):
-    destination = base/ file_type_folder/ date_folder
+    destination = base / file_type_folder / date_folder
     destination.mkdir(parents=True, exist_ok=True)
     return destination
-base = Path.home() / "Downloads"
-result = create_destination(base, "Images", "2025-March")
-print(result)
+
+if __name__ == "__main__":
+    base = Path.home() / "Downloads"
+    result = create_destination(base, "Images", "2025-March")
+    print(result)
