e4ac1046b11bcb32ac636167e17667f345191663
Rania Rashid <raniarashid780@gmail.com>
2026-04-03 05:46:32 -0700

    Add create_destination function

---
+++ file_organizer_automation.py
@@ -0,0 +1,9 @@
+from pathlib import Path
+
+def create_destination(base, file_type_folder, date_folder):
+    destination = base/ file_type_folder/ date_folder
+    destination.mkdir(parents=True, exist_ok=True)
+    return destination
+base = Path.home() / "Downloads"
+result = create_destination(base, "Images", "2025-March")
+print(result)
